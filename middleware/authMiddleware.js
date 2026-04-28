7ed7601e534d5b3c1fe115108b97dba0e9490ef5
Geetha Samminga <geethasamminga@gmail.com>
2026-04-28 17:32:37 +0530

    bearer for authentication

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,11 +4,13 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 function authenticate(req, res, next) {
-  const token = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
+  if (!authHeader) {
     return res.status(401).json({ message: 'No token provided' });
   }
+
+  const token = authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : authHeader;
 
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
